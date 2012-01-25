b40f0c73ecc6b0fa29031d35e84a93cc0c4f14be
David Hernández <dav.viidd94@gmail.com>
2012-01-24 23:37:02 -0600

    Espero que este commit salga bien :)

--- PlacasDF/PlacasDF/CalendarioViewController.m
+++ PlacasDF/PlacasDF/CalendarioViewController.m
@@ -14,14 +14,6 @@
 
 @synthesize imageView = _imageView;
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)didReceiveMemoryWarning
 {
