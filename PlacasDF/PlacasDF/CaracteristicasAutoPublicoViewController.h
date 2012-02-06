0e0965e23998947ad0f80c4855180df0a31b5bb1
Davidman <dav.viidd94@gmail.com>
2012-02-05 23:56:37 -0600

    Cambios A ver si jala

---
+++ PlacasDF/PlacasDF/CaracteristicasAutoPublicoViewController.h
@@ -0,0 +1,15 @@
+//
+//  CaracteristicasAutoPublicoViewController.h
+//  PlacasDF
+//
+//  Created by David Hernández on 2/5/12.
+//  Copyright (c) 2012 Caos Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaracteristicasAutoPublicoViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
---
+++ PlacasDF/PlacasDF/CaracteristicasAutoPublicoViewController.m
@@ -0,0 +1,62 @@
+//
+//  CaracteristicasAutoPublicoViewController.m
+//  PlacasDF
+//
+//  Created by David Hernández on 2/5/12.
+//  Copyright (c) 2012 Caos Inc. All rights reserved.
+//
+
+#import "CaracteristicasAutoPublicoViewController.h"
+
+@implementation CaracteristicasAutoPublicoViewController
+
+@synthesize scrollView = _scrollView;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+ // Implement loadView to create a view hierarchy programmatically, without using a nib.
+ - (void)loadView
+ {
+ }
+ */
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [_scrollView setAlwaysBounceVertical:YES];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
